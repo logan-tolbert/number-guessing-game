265055c0c46c68da66cdc6c10ed95014b8d63ba2
logan-tolbert <logo88205+git@gmail.com>
2024-08-24 12:32:55 -0500

    :tada: Add initial project files and code

---
+++ GuessTheMagicNumber/ConsoeUI/ConsoleFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class ConsoleFunctions
+    {
+        public static string GetName()
+        {
+            Program.DisplayGetName();
+            var name = Console.ReadLine()!;
+            return name;
+       }
+    }
+}
---
+++ GuessTheMagicNumber/ConsoeUI/Program.Messages.cs
@@ -0,0 +1,22 @@
+﻿partial class Program
+{
+    public static void DisplayHeader()
+    {
+        Console.WriteLine("Welcome to Guess the Magic Number!");
+    }
+
+    public static void DisplayGetName()
+    {
+        Console.Write("Please enter your name: ");
+    }
+
+    public static void DisplayRules(string name)
+    {
+        Console.Clear();
+        Console.WriteLine($"Hello, {name}.");
+        Console.WriteLine("The rules to this game are simple.");
+        Console.WriteLine("I have a number between 1 and 100 stored in memory.");
+        Console.WriteLine("Do you think you could guess the number?");
+        Console.WriteLine("The number of attempts before failure depends on the difficulty selected.");
+    }
+}
